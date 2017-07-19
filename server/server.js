7df64f8353197d717bb6b5353dd149b83b2750ba
cxg5940 <clint_garrison@homedepot.com>
2017-07-19 00:04:46 -0400

    added angular material layout, server, and auth logic using cookies

---
+++ server/server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var cookieParser = require('cookie-parser')
+
+app.use(cookieParser());
+
+app.get('/', function(req, res) {
+    console.log(req.cookies)
+    // check if client sent cookie
+    var cookie = req.cookies.authenticatedUser;
+    if (cookie === undefined)
+    {
+      // no: set a new cookie
+      // res.cookie('authenticatedUser','youAreYou', { maxAge: 900000, httpOnly: true });
+      // console.log('cookie created successfully');
+      res.sendFile(path.resolve(".") + '/client/authenticate.html');
+    }
+    else
+    {
+      // yes, cookie was already present
+      console.log('cookie exists', cookie);
+      res.sendFile(path.resolve(".") + '/client/index.html');
+    }
+});
+
+// app.use(express.static(path.resolve(".") + '/client'));
+
+app.listen(9090);
