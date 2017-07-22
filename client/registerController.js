1afaf185c6690b09f3af18743fce9c60bdc4af0e
cxg5940 <clint_garrison@homedepot.com>
2017-07-22 16:59:37 -0400

    added some form validation to registration

--- client/registerController.js
+++ client/registerController.js
@@ -1,5 +1,30 @@
-var app = angular.module("cfbPicker", ['ngMaterial']);
+'use strict'
 
-app.controller("registerController", function($scope) {
-    $scope.test = "test";
+var app = angular.module("cfbPicker", ['ngMaterial'])
+
+app.controller("registerController", function($scope, $mdDialog) {
+    $scope.test = "test"
+    $scope.user = {userName: "", email: "", passwordOne: "", passwordTwo: "", }
+    $scope.register = function() {
+        console.log('regsiter')
+        if($scope.user.passwordOne !== $scope.user.passwordTwo){
+          console.log('p nope')
+          $scope.showAlert()
+          $scope.user.passwordOne = ""
+          $scope.user.passwordTwo = ""
+        }
+
+        console.log($scope.user)
+    };
+
+    $scope.showAlert = function(ev) {
+      $mdDialog.show(
+        $mdDialog.alert()
+          .parent(angular.element(document.querySelector('#popupContainer')))
+          .clickOutsideToClose(true)
+          .title('Passwords do not match')
+          .ok('OK')
+          .targetEvent(ev)
+      );
+  };
 });
