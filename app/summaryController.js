1d3dd54c7f5b437146e459a3d7f713713d447342
cxg5940 <clint_garrison@homedepot.com>
2017-07-26 10:28:39 -0400

    moved side nav outside of ng-view to reduce duplicate code

--- app/summaryController.js
+++ app/summaryController.js
@@ -4,6 +4,7 @@ app.controller("summaryController", function ($scope, $http, $location) {
     $scope.greeting = "Summary Controller";
 
     $scope.navigateTo = function ( path ) {
+      console.log('nav')
       $location.path( path );
     };
 });
