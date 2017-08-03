662a6a86493f7a7be348b2c1fef889eb0dead4ef
cxg5940 <clint_garrison@homedepot.com>
2017-08-02 23:34:15 -0400

    reverting auth stuff

--- app/authService.js
+++ app/authService.js
@@ -1,9 +1,9 @@
 var app = angular.module("cfbPicker")
 
 app.service('authService', function(){
-    var userIsAuthenticated = true;
+    var userIsAuthenticated = false;
 
-    var userName = 'lucy'
+    var userName = ''
 
     this.setUserAuthenticated = function(value){
       userIsAuthenticated = value;
