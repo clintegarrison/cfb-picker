6854af6799bcc639bf8fc4fe8849fdca5e364efc
Clint Garrison <clintegarrison@gmail.com>
2018-01-04 16:08:13 -0500

    week 16

--- app/resultsController.js
+++ app/resultsController.js
@@ -184,6 +184,17 @@ app.controller("resultsController", function ($scope, $http) {
                                     console.log(response)
                                     $scope.isLoading = false
                                   });
+                                  $http({
+                                    method: 'GET',
+                                    url: '/getResults?weekNumber=16'
+                                  }).then(function successCallback(response) {
+                                      console.log(typeof response.data)
+                                      $scope.weekSixteenResults = response.data
+                                      $scope.isLoading = false
+                                    }, function errorCallback(response) {
+                                      console.log(response)
+                                      $scope.isLoading = false
+                                    });
   }
 
   $scope.getResults()
